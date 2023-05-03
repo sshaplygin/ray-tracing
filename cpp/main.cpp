047bf9d2a1825b8fd11504aeaa29da5107a34073
Saimon Shaplygin <semyuon@gmail.com>
2023-05-04 00:54:08 +0400

    add: chapter 3

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,19 +1,34 @@
 #include <iostream>
-#include "vec3.h"
+#include "ray.h"
+
+vec3 color(const ray &r) {
+    vec3 unit_direction = unit_vector(r.direction());
+    float t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - t)*vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+}
 
 int main() {
     // Image
-    const int image_width = 256;
-    const int image_height = 256;
+    const int image_width = 200;
+    const int image_height = 100;
 
     // Render
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
+    vec3 lower_left_corner(-2.0, -1.0, -1.0);
+    vec3 horizontal(4.0, 0.0, 0.0);
+    vec3 vertical(0.0, 2.0, 0.0);
+    vec3 origin(0.0, 0.0, 0.0);
+
     for (int j = image_height-1; j >= 0; j--) {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; i++) {
-            vec3 col(float(i) / float(image_width), float(j) / float(image_height), 0.25);
-            
+            float u = float(i) / float(image_width);
+            float v = float(j) / float(image_height);
+
+            ray r(origin, lower_left_corner + u * horizontal + v * vertical);
+            vec3 col = color(r);
+
             int ir = static_cast<int>(255.999 * col[0]);
             int ig = static_cast<int>(255.999 * col[1]);
             int ib = static_cast<int>(255.999 * col[2]);
