bb4b4053991d7ecdb8fb5ba95f14e929f713a7a1
Saimon Shaplygin <semyuon@gmail.com>
2023-05-21 01:18:01 +0400

    add: hit sphere

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 #include "ray.h"
 
+bool hit_sphere(const vec3& center, float redius, const ray& r) {
+    vec3 oc = r.origin() - center;
+    float a = dot(r.direction(), r.direction());
+    float b = 2.0 * dot(oc, r.direction());
+    float c = dot(oc, oc) - radius * redius;
+    float discriminant = b * b - 4 * a * c;
+    return (discriminant > 0);
+}
+
 vec3 color(const ray &r) {
     vec3 unit_direction = unit_vector(r.direction());
     float t = 0.5 * (unit_direction.y() + 1.0);
