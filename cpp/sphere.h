1e0e941fe53967ef53c9be50f771343ee01dff62
Saimon Shaplygin <semyuon@gmail.com>
2023-09-14 11:59:08 +0400

    add: hitable_list and hitable

--- cpp/sphere.h
+++ cpp/sphere.h
@@ -7,14 +7,14 @@ class sphere: public hitable {
     public:
         sphere() {}
         sphere(vec3 cen, float r): center(cen), radius(r) {};
-        virtual bool hit(const ray& r, float tmin) const;
+        virtual bool hit(const ray& r, float tmin, float tmax, hit_record& rec) const;
         vec3 center;
         float radius;
 };
 
-bool sphere::hit(const ray &r, float t_min, float t_max, hit_record& rec) const {
+bool sphere::hit(const ray& r, float t_min, float t_max, hit_record& rec) const {
     vec3 oc = r.origin() - center;
-    
+
     float a = dot(r.direction(), r.direction());
     float b = dot(oc, r.direction());
     float c = dot(oc, oc) - radius * radius;
@@ -22,6 +22,23 @@ bool sphere::hit(const ray &r, float t_min, float t_max, hit_record& rec) const 
     float discriminant = b*b - a * c;
     if (discriminant <= 0) {
         return false;
+    }
+    float temp = (-b - sqrt(b*b-a*c))/a;
+    if (temp < t_max && temp > t_min) {
+        rec.t = temp;
+        rec.p = r.point_at_parameter(rec.t);
+        rec.nornal = (rec.p - center) / radius;
+
+        return true;
+    }
+
+    temp = (-b + sqrt(b*b-a*c))/a;
+    if (temp < t_max && temp > t_min) {
+        rec.t = temp;
+        rec.p = r.point_at_parameter(rec.t);
+        rec.nornal = (rec.p - center) / radius;
+
+        return true;
     }
 } 
 
