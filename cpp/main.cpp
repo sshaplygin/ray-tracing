60da9e44dba5e36d4daa6ca8a7b996d60586dd27
Saimon Shaplygin <semyuon@gmail.com>
2023-09-23 00:09:10 +0400

    ref: main options

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -54,8 +54,8 @@ int main() {
     camera cam;
 
     cam.aspect_ratio = 16.0 / 9.0;
-    cam.image_width = 100;
-    cam.samples_per_pixel = 10;
+    cam.image_width = 1200;
+    cam.samples_per_pixel = 500;
     cam.max_depth = 50;
 
     cam.vfov = 20;
